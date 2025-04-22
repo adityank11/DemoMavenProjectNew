d3bb61f8d33708b845521606abdfa35d19c65d27
AdityaKelkar1106 <kelkaradityan14@gmail.com>
2025-04-22 22:53:08 +0530

    Initial commit#1

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -5,5 +5,6 @@ import javax.crypto.spec.PSource;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("boo");
     }
 }
